34397d980a8e0369e548fd12f46fdf840b868113
ThomasThePro4 <Thomasharrybow987@gmail.com>
2018-06-06 17:20:03 +0100

    Website

---
+++ htdocs/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<center>
+<h1>MuzzFuzz</h1>
+<title>MuzzFuzz </title>
+
+
+
+<body background="black.JPG">
+	<h2>Dance Till your dead.</h2>
+	<img src="https://media1.tenor.com/images/6bf71c857a5c14ca9f94464d325a7684/tenor.gif?itemid=10061170" width="1280px" height="720px">
+	<embed src="Dance till Youre Dead FULL REMIX Bass Boosted" autostart="true" loop="true" width="2" height="0">
+	
